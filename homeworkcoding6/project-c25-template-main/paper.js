86b7ee0a42d4122245633880887d0110774abfcb
chirayu10 <83503597+chirayu10@users.noreply.github.com>
2021-05-21 12:06:04 +0530

    Add files via upload

---
+++ homeworkcoding6/project-c25-template-main/paper.js
@@ -0,0 +1,28 @@
+class paper{
+    constructor(x,y){
+    var options={
+        isStatic:false,
+        restitution:0.3,
+        friction:0,
+        density:1.2
+    }
+    this.body=Bodies.rectangle(x,y,50,50,options)
+
+    this.width=50
+    this.height=50
+    this.image=loadImage("paper.png")
+    this.body.scale=2
+    World.add(world,this.body)
+   
+
+}
+display(){
+    var Pos=this.body.position
+push();
+    translate(Pos.x, Pos.y);
+    rotate(this.body.angle);
+    imageMode(CENTER);
+    image(this.image,0,0,this.width,this.height);
+pop();
+}
+}
